647fda6c216f2f7566db1a1ca5567190b3f55845
sarwanputeh <bazzarm.sarwan@gmail.com>
2019-07-03 17:22:14 +0700

    update

--- server/src/app.js
+++ server/src/app.js
@@ -1,48 +1,80 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
+
+const config = require('./config/config')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/status', function (req,res) {
-    res.send('Hello nodejs server')
+require('./routes')(app)
+
+app.get('/status',function(req, res){
+    res.send(' Hello nodejs server')
 })
 
-app.get('/hello/:person', function(req,res) {
-    console.log.log('hello -' + req.params.person)
+app.get('/hello/:person',function (req, res){
+    console.log('hello-' + req.params.person)
     res.send('sey hello with' + req.params.person)
 })
 
-//get user by id
-app.get('/user/:userId', function(req,res){
-    res.send('see data user' + req.params.userId)
+app.post('/hello', function(req, res){
+    res.send('ok you post-' + req.body.name)
+})
+let port =process.env.PORT||config.port
+
+sequelize.sync({force:false}).then(()=>{
+    app.listen(port, function(){
+        console.log('Server running on')
+    })
 })
 
-//get all user
-app.get('/users', function(req, res){
-    res.send('requie data user')
-})
 
-//create user
-app.post('/user/', function(req, res){
-    res.send('create user' + JSON.stringify(req.body))
-})
 
-//edit user
-app.put('/user/:userId', function(req,res) {
-    res.send('edit user' + req.params.userId + ':'+
-    JSON.stringify(req.body))
-})
 
-//delete user
-app.delete('/user/:userId', function(req,res){
-    res.send('Delete' + req.params.userId+ ':'+
-    JSON.stringify(req.body))
-})
 
-let port = 8081
-app.listen(port, function () {
-    console.log('server running on' + port)
-})
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: true}))
+
+// app.get('/status', function (req,res) {
+//     res.send('Hello nodejs server')
+// })
+
+// app.get('/hello/:person', function(req,res) {
+//     console.log.log('hello -' + req.params.person)
+//     res.send('sey hello with' + req.params.person)
+// })
+
+// //get user by id
+// app.get('/user/:userId', function(req,res){
+//     res.send('see data user' + req.params.userId)
+// })
+
+// //get all user
+// app.get('/users', function(req, res){
+//     res.send('requie data user')
+// })
+
+// //create user
+// app.post('/user/', function(req, res){
+//     res.send('create user' + JSON.stringify(req.body))
+// })
+
+// //edit user
+// app.put('/user/:userId', function(req,res) {
+//     res.send('edit user' + req.params.userId + ':'+
+//     JSON.stringify(req.body))
+// })
+
+// //delete user
+// app.delete('/user/:userId', function(req,res){
+//     res.send('Delete' + req.params.userId+ ':'+
+//     JSON.stringify(req.body))
+// })
+
+// let port = 8081
+// app.listen(port, function () {
+//     console.log('server running on' + port)
+// })
---
+++ server/src/models/User.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User',{
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        name: DataTypes.STRING,
+        lastname: DataTypes.STRING,
+        status: DataTypes.STRING,
+        type: DataTypes.STRING
+    })
+    return User
+} 
+
+
+
+
